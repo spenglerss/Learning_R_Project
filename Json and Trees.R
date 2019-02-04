0329009d4ef49bfd09667a280cfc23333f86b62a
spenglerss <sandeshsadalge@gmail.com>
2019-02-03 19:24:09 -0500

    Experiments for JSON (short) and Creating and interpreting Trees

---
+++ Json and Trees.R
@@ -0,0 +1,34 @@
+# Some JSON experiments
+
+library(jsonlite)
+
+test <- fromJSON("TestFileTheGallery.json")
+
+menu <- test[["menu"]]
+
+variants <- test[["menu"]][["variants"]]
+
+flattened_test <- flatten(test, recursive = TRUE)
+
+# This is exercise to create Trees based on this blog post: https://www.datacamp.com/community/tutorials/decision-trees-R
+
+
+library(ISLR)
+library(tree)
+
+data(package="ISLR")
+
+carseats <- Carseats
+
+head(carseats)
+names(carseats)
+hist(carseats$Sales)
+
+# Going to create a new variable alled "HIGH" based on the value in Sales:
+
+High <- ifelse(carseats$Sales <=8, "No", "Yes")
+carseats <- data.frame(carseats, High)
+
+#do a tree without Sales because High is based on Sales
+tree.carseats = tree(High~.-Sales, data = carseats)
+summary(tree.carseats)
