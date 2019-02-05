606a99f31da4164c8006bbd52ae31df8fcd26bc8
Sandesh Sadalge <ssadalge@educationdynamics.com>
2019-02-04 19:07:36 -0500

    cc

---
+++ Clustering Analysis DataCamp.R
@@ -0,0 +1,7 @@
+#   1. Pre-process : Prepare data
+#   2. Select Similarity Measure (metric, euclidiean, jaccardian, etc..., method, type)
+#   3. Cluster
+#   4. Analyze results
+#   Go back to #1 and repeat!
+
+#   Linkages! Dendrograms, Jaccard Index
